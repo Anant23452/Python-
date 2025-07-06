3df12fddfec47d2b4a5555a352e7e652d1f21712
anantkpatel <imagine231216@gmail.com>
2025-07-06 08:59:56 +0530

    car

--- Loops/For loop/String/dictonary.py
+++ Loops/For loop/String/dictonary.py
@@ -3,3 +3,7 @@ for key in course:
     # print(key, course[key], end=" ")
     print(key, end=" ")
     print(course[key], end=" ")
+# printing both key and value in a dictionary
+cars={1:"audi",2:"bmw",3:"benz",4:"toyota"}
+for x,y in cars.items():
+    print(x,y, end=" ")
