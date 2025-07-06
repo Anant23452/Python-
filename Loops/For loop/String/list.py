553e487115847b6a1660188a1b0b702094b24c57
anantkpatel <imagine231216@gmail.com>
2025-07-06 08:49:37 +0530

    list

--- Loops/For loop/String/list.py
+++ Loops/For loop/String/list.py
@@ -1,1 +1,12 @@
-car=[]
+cars=['audi','bmw','benz','toyota']
+for car in cars:
+ print(car, end=" ")
+
+#  list cpmprehension= [car for car in cars]
+cars =['audi','bmw','benz','toyota']
+[print(car, end=" ")for car in cars]
+
+#range
+car =['audi','bmw','benz','toyota']
+for i in range(len(car)):
+    print(car[i], end=" ")
