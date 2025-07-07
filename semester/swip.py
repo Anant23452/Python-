ba637a4de3d84b769bedf231592934377fed4053
anantkpatel <imagine231216@gmail.com>
2025-07-07 22:33:31 +0530

    swap

---
+++ semester/swip.py
@@ -0,0 +1,12 @@
+x=int(input("Enter a number"))
+y=int(input("Enter a number"))
+print("xis value",x)
+print("yis value",y)
+
+temp = x
+x = y
+y = temp
+
+
+print("xis after swapping",x)
+print("yis after swapping",y)
